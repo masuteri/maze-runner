9d626718cab71eea7738c33cedc89a66f901efc1
Bamrit03 <113582891+Bamrit03@users.noreply.github.com>
2023-11-10 12:43:55 +0200

    moved map in unity

---
+++ Assets/Tiled/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="16" tileheight="16" tilecount="192" columns="24">
+ <image source="map asset/Dirt-&amp;-Grass-2D-Platform-Tileset_by_AxulArt/grass tileset.png" width="384" height="128"/>
+</tileset>
